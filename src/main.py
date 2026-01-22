77206b1463861a243536366a02732cb50905e989
JBK <jaebin0815@naver.com>
2026-01-22 15:47:43 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from scraper.kleague_scraper import collect_kleague_data
+from scraper.kleague_match_scraper import collect_kleague_data
 from saver.csv_saver import save_to_csv
 
 def main() -> None:
@@ -6,8 +6,8 @@ def main() -> None:
     메인 실행 함수
     """
 
-    year, dataset = collect_kleague_data(2024, "K리그1")
-    save_to_csv(year, dataset)
+    year_label, dataset = collect_kleague_data([2023, 2025], "K리그1")
+    save_to_csv(year_label, dataset)
 
 
 if __name__ == "__main__":
