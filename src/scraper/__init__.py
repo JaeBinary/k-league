a6daab6ce2c7fa33a3acb0c3faca4e95e50085dd
JBK <jaebin0815@naver.com>
2026-01-22 18:12:21 +0900

    Update __init__.py

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,5 +1,7 @@
 # ./src/scraper/__init__.py
 
 from .kleague_match_scraper import collect_kleague_match_data
+from .kleague_preview_scraper import collect_kleague_preview_data
+from .scraper import fetch_page
 
-__all__ = ["collect_kleague_match_data"]
+__all__ = ["collect_kleague_match_data", "collect_kleague_preview_data", "fetch_page"]
