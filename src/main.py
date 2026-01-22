e3729f7f28376ebb9a551d9ada8b6d3fe205f90d
JBK <jaebin0815@naver.com>
2026-01-22 23:35:18 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 from scraper.kleague_match_scraper import collect_kleague_match_data
 from scraper.kleague_preview_scraper import collect_kleague_preview_data
 from saver.csv_saver import save_to_csv
+from saver.db_saver import save_to_db
 
 def main() -> None:
     """
     메인 실행 함수
     """
 
-    # match 데이터 수집 및 CSV 저장
+    # match 데이터 수집 후 CSV 저장 및 DB 변환
     dataset, file_name = collect_kleague_match_data([2023, 2025], "K리그1")
-    save_to_csv(dataset, file_name)
+    csv_file_path = save_to_csv(dataset, file_name)
+    save_to_db(csv_file_path, table_name="matche")
 
-    # preview 데이터 수집 및 CSV 저장
+    # preview 데이터 수집 후 CSV 저장 및 DB 변환
     dataset, file_name = collect_kleague_preview_data([2023, 2025], "K리그1")
-    save_to_csv(dataset, file_name)
+    csv_file_path = save_to_csv(dataset, file_name)
+    save_to_db(csv_file_path, table_name="preview")
 
 if __name__ == "__main__":
     main()
