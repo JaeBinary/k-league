01d71ef89a4b23d5073eef3b1c8111e6cfc7d134
JBK <jaebin0815@naver.com>
2026-01-25 03:25:18 +0900

    Update __init__.py

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,7 +1,8 @@
 # ./src/scraper/__init__.py
 
+from .jleague_match_scraper import collect_jleague_match_data
 from .kleague_match_scraper import collect_kleague_match_data
 from .kleague_preview_scraper import collect_kleague_preview_data
-from .scraper import fetch_page
+from .scraper import fetch_page, setup_chrome_driver
 
-__all__ = ["collect_kleague_match_data", "collect_kleague_preview_data", "fetch_page"]
+__all__ = ["collect_jleague_match_data", "collect_kleague_match_data", "collect_kleague_preview_data", "fetch_page", "setup_chrome_driver"]
