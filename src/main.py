b54844f94ee4b346cd1b8a3f6a90bbbbc2f32c2e
JBK <jaebin0815@naver.com>
2026-01-25 03:25:48 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from scraper.jleague_match_scraper import collect_jleague_match_data
 from scraper.kleague_match_scraper import collect_kleague_match_data
 from scraper.kleague_preview_scraper import collect_kleague_preview_data
 from saver.csv_saver import save_to_csv
@@ -8,15 +9,24 @@ def main() -> None:
     메인 실행 함수
     """
 
-    # match 데이터 수집 후 CSV 저장 및 DB 변환
+    # K리그 match 데이터 수집 후 CSV 저장 및 DB 변환
+    """
     dataset, file_name = collect_kleague_match_data([2023, 2025], ["K리그1", "K리그2"])
     csv_file_path = save_to_csv(dataset, file_name)
     save_to_db(csv_file_path, table_name="match")
+    """
 
-    # preview 데이터 수집 후 CSV 저장 및 DB 변환
-    #dataset, file_name = collect_kleague_preview_data([2023, 2025], ["K리그1", "K리그2"])
-    #csv_file_path = save_to_csv(dataset, file_name)
-    #save_to_db(csv_file_path, table_name="preview")
+    # K리그 preview 데이터 수집 후 CSV 저장 및 DB 변환
+    """
+    dataset, file_name = collect_kleague_preview_data([2023, 2025], ["K리그1", "K리그2"])
+    csv_file_path = save_to_csv(dataset, file_name)
+    save_to_db(csv_file_path, table_name="preview")
+    """
+
+    # J리그 match 데이터 수집 후 CSV 저장 및 DB 변환
+    dataset, file_name = collect_jleague_match_data([2023, 2025], ["J리그1", "J리그2"])
+    csv_file_path = save_to_csv(dataset, file_name)
+    save_to_db(csv_file_path, table_name="match")
 
 if __name__ == "__main__":
     main()
